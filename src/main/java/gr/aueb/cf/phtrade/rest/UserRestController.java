c5520fa2dc2fbfc08a516051556a2a35c3780b75
digeorgiou <digeorgiou1990@gmail.com>
2025-06-07 02:29:10 +0300

    user controller

--- src/main/java/gr/aueb/cf/phtrade/rest/UserRestController.java
+++ src/main/java/gr/aueb/cf/phtrade/rest/UserRestController.java
@@ -1,4 +1,10 @@
 package gr.aueb.cf.phtrade.rest;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Path;
+
+@ApplicationScoped
+@Path("/users")
 public class UserRestController {
+
 }
